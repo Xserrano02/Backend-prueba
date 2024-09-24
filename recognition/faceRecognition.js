2aa613b3c4ca640fa33cc739b4e4ed42d7c6d061
Xserrano02 <74936443+Xserrano02@users.noreply.github.com>
2024-09-24 14:11:27 -0600

    Update faceRecognition.js

--- recognition/faceRecognition.js
+++ recognition/faceRecognition.js
@@ -3,7 +3,7 @@ const canvas = require('canvas');
 const path = require('path');
 const { Canvas, Image, ImageData } = canvas;
 
-// Monkey-patch para usar canvas en Node.js
+
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 const loadModels = async () => {
@@ -15,14 +15,13 @@ const loadModels = async () => {
 const compareFaces = async (selfieBase64, documentBase64) => {
   await loadModels();
 
-  // Convertir las imágenes base64 a buffer
+
   const selfieBuffer = Buffer.from(selfieBase64, 'base64');
   const documentBuffer = Buffer.from(documentBase64, 'base64');
 
   const imgSelfie = await canvas.loadImage(selfieBuffer);
   const imgDocument = await canvas.loadImage(documentBuffer);
 
-  // Detectar los rostros en ambas imágenes
   const detectionsSelfie = await faceapi.detectSingleFace(imgSelfie).withFaceLandmarks().withFaceDescriptor();
   const detectionsDocument = await faceapi.detectSingleFace(imgDocument).withFaceLandmarks().withFaceDescriptor();
 
@@ -30,7 +29,6 @@ const compareFaces = async (selfieBase64, documentBase64) => {
     throw new Error('No se detectaron rostros en una o ambas imágenes.');
   }
 
-  // Comparar descriptores faciales
   const distance = faceapi.euclideanDistance(detectionsSelfie.descriptor, detectionsDocument.descriptor);
   return distance < 0.6;
 };
